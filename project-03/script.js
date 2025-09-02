90bf02de4c7f8a42efbe98e723ec4a6cf6eee890
Daniel Sinkpon <danielsinkpon@gmail.com>
2025-09-02 11:34:20 -0400

    Add alternating semi-circular movement pattern to enemy

--- project-03/script.js
+++ project-03/script.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
-const numberOfEnemies = 30;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -15,38 +15,26 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-<<<<<<< HEAD
-        this.image.src = './assets/enemy1.png';
-=======
-        this.image.src = './assets/enemy2.png';
->>>>>>> bacae72 (Create Sprite sheet for enemy movement)
+        this.image.src = './assets/enemy3.png';
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-<<<<<<< HEAD
+        this.angle = Math.random() * 500;
+        this.angleSpeed = Math.random() * 1.5 + 0.5;
+        // this.curve = Math.random() * 200 + 50;
     }
 
     update(){
-        this.x += this.speed;
-        this.y +=  this.curve * Math.sin(this.angle); 
-        // this.angle += this.angleSpeed;
-=======
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 7;
-    }
-
-    update(){
-        this.x -= this.speed;
-        this.y +=  this.curve * Math.sin(this.angle); 
-        this.angle += this.angleSpeed;
->>>>>>> bacae72 (Create Sprite sheet for enemy movement)
+        this.x = canvas.width/2 * Math.cos(this.angle * Math.PI /200) + (canvas.width/2 - this.width/2);
+        this.y = canvas.height/2 * Math.sin(this.angle * Math.PI /300) + (canvas.height/2 - this.height/2);
+        // this.y += Math.random() * 5 - 2.5;
+        this.angle += this.angleSpeed
         if (this.x + this.width < 0){
             this.x = canvas.width;
         }
