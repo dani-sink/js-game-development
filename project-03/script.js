1a4400b77c562d8679e5a49a2aee878dc6af22bd
Daniel Sinkpon <danielsinkpon@gmail.com>
2025-09-04 08:12:55 -0400

    Update dev

--- project-03/script.js
+++ project-03/script.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
-const numberOfEnemies = 30;
+const numberOfEnemies = 10;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -15,38 +15,32 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-<<<<<<< HEAD
-        this.image.src = './assets/enemy1.png';
-=======
-        this.image.src = './assets/enemy2.png';
->>>>>>> bacae72 (Create Sprite sheet for enemy movement)
+        this.image.src = './assets/enemy4.png';
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * (canvas.width);
+        this.newY = Math.random() * (canvas.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-<<<<<<< HEAD
+        this.interval = Math.floor(Math.random() * 250 + 50);
     }
 
     update(){
-        this.x += this.speed;
-        this.y +=  this.curve * Math.sin(this.angle); 
-        // this.angle += this.angleSpeed;
-=======
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 7;
-    }
+        if (gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
 
-    update(){
-        this.x -= this.speed;
-        this.y +=  this.curve * Math.sin(this.angle); 
-        this.angle += this.angleSpeed;
->>>>>>> bacae72 (Create Sprite sheet for enemy movement)
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/120;
+        this.y -= dy/120;
+
         if (this.x + this.width < 0){
             this.x = canvas.width;
         }
